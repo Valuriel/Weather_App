bfe0c18eb03f632b7a35d49b374c61d13e34ac20
Valuriel <christian.poetter@hotmail.de>
2021-09-06 13:57:12 +0300

    added update UI functionality

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,37 @@
 // DOM manipulation and event handling
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+
+const updateUI = (data) => {
+
+    const cityDetails = data.cityDetails;
+    const weather = data.weather;
+
+    // update details template
+    details.innerHTML = `
+        <h5 class="my-3">${cityDetails.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4 my-4">
+            <span>${weather.Temperature.Metric.Value}</span>
+            <span>&deg;C</span>
+        </div>
+    `;
+
+    // remove the d-none class if present
+    if(card.classList.contains('d-none')){
+        card.classList.remove('d-none');
+    }
+
+
+};
 
 const updateCity = async (city) => {
 
     const cityDetails = await getCity(city);
     const weather = await getWeather(cityDetails.Key);
 
-    return {
-        cityDetails: cityDetails,
-        weather: weather
-    };
+    return { cityDetails,weather };
 
 };
 
@@ -23,6 +45,6 @@ cityForm.addEventListener('submit', e => {
 
     // Update the UI with the new city
     updateCity(city)
-    .then(data => console.log(data))
+    .then(data => updateUI(data))
     .catch(err => console.log(err));
 });
